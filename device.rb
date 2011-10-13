53f389f3270211f3f899d67ecd0d4e46c60d1fa4
Jarrett Baugh <jarrett.baugh@gmail.com>
2011-10-12 17:33:35 -0700

    Fixed boolean error and added can? method

--- device.rb
+++ device.rb
@@ -14,10 +14,10 @@ class Device
   def reset
     case @type
     when :tape
-      @io_type = Device.bin_in & Device.bin_out
+      @io_type = Device.bin_in | Device.bin_out
       num_of_words = 100
     when :disk
-      @io_type = Device.bin_in & Device.bin_out
+      @io_type = Device.bin_in | Device.bin_out
       num_of_words = 100
     when :card_reader
       @io_type = Device.char_in
@@ -29,7 +29,7 @@ class Device
       @io_type = Device.char_out
       num_of_words = 24
     when :terminal
-      @io_type = Device.char_in & Device.char_out
+      @io_type = Device.char_in | Device.char_out
       num_of_words = 14
     when :paper_tape
       @io_type = Device.char_in
@@ -37,6 +37,10 @@ class Device
     end
 
     @memory = Memory.new(@computer, num_of_words)
+  end
+
+  def can?(io)
+     (io & @io_type) > 0
   end
 
   def self.bin_in;   1; end   # 0001
--- tests/device_test.rb
+++ tests/device_test.rb
@@ -5,17 +5,16 @@ load 'device.rb'
  class DeviceTest < Test::Unit::TestCase
   def setup
     @computer = Computer.new
-    @device = Device.new(@computer, :tape)
   end
   
   def test_initialize
     @dev = Device.new(@computer, :tape)
     assert_equal(100, @dev.memory.storage_amount, "Tape device initialization")
-    assert_equal(Device.bin_in & Device.bin_out, @dev.io_type, "Tape device initialization")
+    assert_equal(Device.bin_in | Device.bin_out, @dev.io_type, "Tape device initialization")
 
     @dev = Device.new(@computer, :disk)
     assert_equal(100, @dev.memory.storage_amount, "Disk device initialization")
-    assert_equal(Device.bin_in & Device.bin_out, @dev.io_type, "Disk device initialization")
+    assert_equal(Device.bin_in | Device.bin_out, @dev.io_type, "Disk device initialization")
 
     @dev = Device.new(@computer, :card_reader)
     assert_equal(16, @dev.memory.storage_amount, "Card reader device initialization")
@@ -31,11 +30,55 @@ load 'device.rb'
 
     @dev = Device.new(@computer, :terminal)
     assert_equal(14, @dev.memory.storage_amount, "Terminal device initialization")
-    assert_equal(Device.char_in & Device.char_out, @dev.io_type, "Terminal device initialization")
+    assert_equal(Device.char_in | Device.char_out, @dev.io_type, "Terminal device initialization")
 
     @dev = Device.new(@computer, :paper_tape)
     assert_equal(14, @dev.memory.storage_amount, "Paper tape device initialization")
     assert_equal(Device.char_in, @dev.io_type, "Paper tape device initialization")
+  end
+
+  def test_can?
+    @dev = Device.new(@computer, :tape)
+    assert_equal(true, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :disk)
+    assert_equal(true, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :card_reader)
+    assert_equal(false, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :card_writer)
+    assert_equal(false, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :line_printer)
+    assert_equal(false, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :terminal)
+    assert_equal(false, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_out), "Device io capabilities")
+
+    @dev = Device.new(@computer, :paper_tape)
+    assert_equal(false, @dev.can?(Device.bin_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.bin_out), "Device io capabilities")
+    assert_equal(true, @dev.can?(Device.char_in), "Device io capabilities")
+    assert_equal(false, @dev.can?(Device.char_out), "Device io capabilities")
   end
 
 end
